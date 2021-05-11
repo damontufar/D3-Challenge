7cf9d920280d3f1697aa0dc272b78e0ab1e3805c
Daniela Marroquín <daniela.montufar18@gmail.com>
2021-05-11 10:57:56 -0500

    Adding Tooltip Code

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -22,51 +22,64 @@ let svg = d3
     .attr("height", svgHeight);
 
 let chartGroup = svg.append("g")
-.att("transform", `translate(${margin.left}, ${margin.top})`);
+.attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // 3. Import Data from data.csv
 
-d3.csv("data.csv").then(function(healthData){
+d3.csv("assets/data/data.csv").then(function(healthData){
     //4. Parse Data
     healthData.forEach(function(data){
         data.state_abbr = +data.abbr;
         data.healtcare = +data.healtcare;
         data.poverty = +data.poverty;
     });
+
+    // 5. Scales
+
+    let xLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(healthData, d => d.poverty)])
+        .range([0, width]);
+
+    let yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(healthData, d => d.healtcare)])
+    .range([height, 0]);
+
+    // 6. Axes
+
+    let bottomAxis = d3.axisBottom(xLinearScale);
+    let leftAxis = d3.axisLeft(yLinearScale);
+
+    // 7. Append the axes to the ChartGroup
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    // 8. Create Circles
+
+    let circlesGroup = chartGroup.selectAll("circle")
+        .data(healthData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healtcare))
+        .attr("r", "15")
+        .attr("fill", "lightblue")
+        .attr("opacity", ".8");
+
+    // 9. Tooltip
+
+    let toolTip = d3.tip()
+        .attr("class", "toolTip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (d.abbr);
+        });
+
+    // 10. Create tooltip in the chart
+
+    chartGroup.call(toolTip);
 });
-
-// 5. Scales
-
-let xLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(healthData, d => d.poverty)])
-    .range([0, width]);
-
-let yLinearScale = d3.scaleLinear()
-.domain([0, d3.max(healthData, d => d.healtcare)])
-.range([height, 0]);
-
-// 6. Axes
-
-let bottomAxis = d3.axisBottom(xLinearScale);
-let leftAxis = d3.axisLeft(yLinearScale);
-
-// 7. Append the axes to the ChartGroup
-
-chartGroup.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(bottomAxis);
-
-chartGroup.append("g")
-    .call(leftAxis);
-
-// 8. Create Circles
-
-let circlesGroup = chartGroup.selectAll("circle")
-    .data(healthData)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xLinearScale(d.poverty))
-    .attr("cy", d => yLinearScale(d.healtcare))
-    .attr("r", "15")
-    .attr("fill", "lightblue")
-    .attr("opacity", ".8");
